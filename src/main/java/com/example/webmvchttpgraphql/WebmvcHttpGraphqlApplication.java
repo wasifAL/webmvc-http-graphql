55a97ddf7bed29952047f81cd4f220b0c860aa3f
Wasif Al Mahmud <wamvmaw@gmail.com>
2022-02-24 18:46:16 +0600

    initialized project

---
+++ src/main/java/com/example/webmvchttpgraphql/WebmvcHttpGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.webmvchttpgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmvcHttpGraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebmvcHttpGraphqlApplication.class, args);
+    }
+
+}
